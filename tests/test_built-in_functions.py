fded7376cdfd79648a1e72df1b0469a4d0be5be6
Anton S. Kosinov <a.s.kosinov@gmail.com>
2016-09-13 20:40:20 +0300

    Any() built-in testing

--- tests/test_built-in_functions.py
+++ tests/test_built-in_functions.py
@@ -40,5 +40,34 @@ class TestAll(unittest.TestCase):
     def test_non_iter_argument(self):
         self.assertRaises(TypeError, lambda x: all(11))
 
+    # assertRaises(exception, callable, *args, **kwds)
+    
     def test_bool_argument(self):
         self.assertRaises(TypeError, lambda x: all(True))
+
+
+class TestAny(unittest.TestCase):
+
+    def test_boolean_arguments(self):
+        self.assertTrue(any([True, True]))
+
+    def test_mixed_arguments(self):
+        self.assertTrue(any([True, False]))
+
+    def test_all_false_elements(self):
+        self.assertFalse(any([False, False]))
+
+    def test_string_as_argument(self):
+        self.assertTrue(any("Blasphemy"))
+
+    def test_empty_list(self):
+        self.assertFalse(any([]))
+
+    # def test_(self):
+    #     self.assertTrue(any([True, True]))
+
+    # def test_(self):
+    #     self.assertTrue(any([True, True]))
+
+    # def test_(self):
+    #     self.assertTrue(any([True, True]))
