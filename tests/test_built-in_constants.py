513aa33cf52a7a8e3f253ad74a49261fc7d78d8b
Anton S. Kosinov <a.s.kosinov@gmail.com>
2017-11-18 16:29:28 +0200

    Equality is not identity test suite

--- tests/test_built-in_constants.py
+++ tests/test_built-in_constants.py
@@ -13,3 +13,26 @@ class TestBuiltInConstants(unittest.TestCase):
     def test_none(self):
         value = None
         self.assertIsNone(value)
+
+
+class TestEqualityQuirks(unittest.TestCase):
+    """Equality is not identity
+
+    """
+
+    def setUp(self):
+        self.arbitrary_numeric_variable = 5.4
+
+    def test_unequality_by_value(self):
+        # these are not the same objecsts
+        self.assertFalse(self.arbitrary_numeric_variable is 5.4)
+
+    def test_equality_by_value(self):
+        self.assertTrue(self.arbitrary_numeric_variable == 5.4)
+
+    def test_equality_by_temp_variable(self):
+        clone_variable = self.arbitrary_numeric_variable
+        self.assertTrue(clone_variable == self.arbitrary_numeric_variable)
+
+    def tearDown(self):
+        del(self.arbitrary_numeric_variable)
