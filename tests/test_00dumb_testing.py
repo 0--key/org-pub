f881da763235f3716608f732b77bec0b8322a797
Anton S. Kosinov <a.s.kosinov@gmail.com>
2021-01-18 12:00:51 +0200

    PEP8

--- tests/test_00dumb_testing.py
+++ tests/test_00dumb_testing.py
@@ -1,27 +1,33 @@
-"""
-Lets discover built-in constants True, False, None and already
-existing testing methods one by one
-
-| Method                    | Checks that          |
-|---------------------------+----------------------+
-| assertEqual(a, b)         | a == b               |
-| assertNotEqual(a, b)      | a != b               |
-| assertTrue(x)             | bool(x) is True      |
-| assertFalse(x)            | bool(x) is False     |
-| assertIs(a, b)            | a is b               |
-| assertIsNot(a, b)         | a is not b           |
-| assertIsNone(x)           | x is None            |
-| assertIsNotNone(x)        | x is not None        |
-| assertIn(a, b)            | a in b               |
-| assertNotIn(a, b)         | a not in b           |
-| assertIsInstance(a, b)    | isinstance(a, b)     |
-| assertNotIsInstance(a, b) | not isinstance(a, b) |
-
-"""
 import unittest
+
+"""
+This is a large comment's block of code. And it is absolutely
+necessary.
+"""
 
 
 class TestTrue(unittest.TestCase):
+
+    """
+    Lets discover built-in constants True, False, None and already
+    existing testing methods one by one
+
+    | Method                    | Checks that          |
+    |---------------------------+----------------------+
+    | assertEqual(a, b)         | a == b               |
+    | assertNotEqual(a, b)      | a != b               |
+    | assertTrue(x)             | bool(x) is True      |
+    | assertFalse(x)            | bool(x) is False     |
+    | assertIs(a, b)            | a is b               |
+    | assertIsNot(a, b)         | a is not b           |
+    | assertIsNone(x)           | x is None            |
+    | assertIsNotNone(x)        | x is not None        |
+    | assertIn(a, b)            | a in b               |
+    | assertNotIn(a, b)         | a not in b           |
+    | assertIsInstance(a, b)    | isinstance(a, b)     |
+    | assertNotIsInstance(a, b) | not isinstance(a, b) |
+
+    """
 
     def test_does_true_exist(self):  # <-- an atomic test case itself
         self.assertTrue(True)  # <-- straight assertion
