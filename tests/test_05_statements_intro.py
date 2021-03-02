19bc41a5efeff30c2d45b9c20e4c950b4be58b77
Anton S. Kosinov <a.s.kosinov@gmail.com>
2021-03-02 10:32:13 +0200

    The explicit enhancements

--- tests/test_05_statements_intro.py
+++ tests/test_05_statements_intro.py
@@ -2,26 +2,33 @@
 
 import unittest
 
-"""The program below is identical by its meaning to previous one, but
-it written in a distinct manner with intention to introduce the
-statement and operator concepts."""
+"""Lets immerse into statement and operators in it.
+
+The program below is identical by its meaning to previous one, but it
+written in a distinct manner with intention to introduce the statement
+and operator concepts, which are the pillars of further studying of
+programming."""
 
 
-class TestFundamentalStatements(unittest.TestCase):
+class TestEqualityStatements(unittest.TestCase):
 
     def test_equality_operator(self):
         # equality operator is double =
         self.assertTrue(True == True)  # <-- equality statements
         self.assertTrue(False == False)  # are inside the brackets
-        # and they evaluated when the program do launch
+        # and they do evaluate when the program do launch
         self.assertTrue(None == None)
 
     def test_non_equality_operator(self):
         # non equality operator is !=
-        self.assertFalse(True != True)
-        self.assertFalse(False != False)
+        self.assertFalse(True != True)  # <-- non-equality statements
+        self.assertFalse(False != False)  # are inside the brackets
+        # and they do evaluate when you're launch the program
         self.assertFalse(None != None)
 
 
-"""You might notice that in this case we assert the ~result~ of
-statement evaluation. """
+"""
+You might notice that in these cases we:
+ - assert the ~result~ of statement evaluation;
+ - disclose the magic above only two operators
+"""
