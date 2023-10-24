8e6ecf7a70a841843a87bd64df1069df476044e1
Anton S. Kosinov <a.s.kosinov@gmail.com>
2023-10-24 09:52:34 +0100

    Several crucial enhancements

--- tests/test_00_primal_testing_oversimplified.py
+++ tests/test_00_primal_testing_oversimplified.py
@@ -2,12 +2,17 @@ import unittest
 
 
 class TestLanguagePrimals(unittest.TestCase):
+    """True, False and None are the basement of any logic.
+
+    Thus theirs derivatitve methods of assertion are fundamental as
+    well.
+    """
 
     def test_does_true_ever_exist(self):
-        self.assertTrue(True)
+	self.assertTrue(True)
 
     def test_does_false_ever_exist(self):
-        self.assertFalse(False)
+	self.assertFalse(False)
 
     def test_does_none_ever_exist(self):
-        self.assertIsNone(None)
+	self.assertIsNone(None)
